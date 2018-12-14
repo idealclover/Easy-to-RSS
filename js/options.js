3ff1ffed13f2e17aed683b09cd33cae4748cde8a
idealclover <shadowspacex@163.com>
2018-12-14 14:16:01 +0800

    :bug: fixed #6 , #5

--- js/options.js
+++ js/options.js
@@ -1,21 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log(localStorage["serveraddress"]);
-    var serveraddress = localStorage["serveraddress"];
-    if(serveraddress !== undefined){
-        var address = document.getElementById("serveraddress");
-        chrome.storage.local.get("serveraddress", function(rst) {
-            if(rst["serveraddress"] !== undefined){
-                address.value = rst["serveraddress"];
-            }
-        });
-    }
+    var address = document.getElementById("serveraddress");
+    chrome.storage.local.get("serveraddress", function(rst) {
+        if(rst["serveraddress"] !== undefined){
+            address.value = rst["serveraddress"];
+        }
+    });
 });
 
 document.getElementById("save").addEventListener('click', function() {
-    console.log(document.getElementById("serveraddress").value);
     var address = document.getElementById("serveraddress").value;
     chrome.storage.local.set({'serveraddress': address}, function() {
-        // alert("设置成功！");
         window.close();
     });
 });
